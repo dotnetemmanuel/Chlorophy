04484c9a4e0e6593fc8dbc83cdd5ed35fb2023fd
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-03-15 09:30:26 +0100

    removed unnecessary comment

--- ChlorophyGUITests/ChlorophyGUITests/ApplicationFacade/WateringMessageFacade.cs
+++ ChlorophyGUITests/ChlorophyGUITests/ApplicationFacade/WateringMessageFacade.cs
@@ -12,25 +12,22 @@ namespace ChlorophyGUITests.Components
             {
                 if (plant.WateringDate != null)
                 {
-                    if (plant.WateringDate != null)
-                    {
-                        TimeSpan difference = MainPage.Today.Subtract((DateTime)plant.WateringDate);
-                        int dateDifference = difference.Days;
+                    TimeSpan difference = MainPage.Today.Subtract((DateTime)plant.WateringDate);
+                    int dateDifference = difference.Days;
 
-                        if (dateDifference != plant.WateringFrequency || dateDifference > 0)
+                    if (dateDifference != plant.WateringFrequency || dateDifference > 0)
+                    {
+                        if (plant.WateringFrequency - dateDifference == 1)
                         {
-                            if (plant.WateringFrequency - dateDifference == 1)
-                            {
-                                wateringReminder = (plant.WateringFrequency - dateDifference).ToString() + " day before watering";
-                            }
-                            else if (plant.WateringFrequency - dateDifference > 1)
-                            {
-                                wateringReminder = (plant.WateringFrequency - dateDifference).ToString() + " days before watering";
-                            }
-                            else if (dateDifference >= plant.WateringFrequency || dateDifference <= 0)
-                            {
-                                wateringReminder = "I need water!";
-                            }
+                            wateringReminder = (plant.WateringFrequency - dateDifference).ToString() + " day before watering";
+                        }
+                        else if (plant.WateringFrequency - dateDifference > 1)
+                        {
+                            wateringReminder = (plant.WateringFrequency - dateDifference).ToString() + " days before watering";
+                        }
+                        else if (dateDifference >= plant.WateringFrequency || dateDifference <= 0)
+                        {
+                            wateringReminder = "I need water!";
                         }
                     }
                 }
